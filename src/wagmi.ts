23aef0fbaa24ad1b001da36383acff3e4373e532
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2025-12-19 22:42:22 +0200

    fix: add base account connector

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,8 +1,11 @@
 import { createConfig, http } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
+import { baseAccount } from "wagmi/connectors";
+
 
 export const config = createConfig({
 	chains: [mainnet, sepolia],
+	connectors: [baseAccount()],
 	transports: {
 		[mainnet.id]: http(),
 		[sepolia.id]: http(),
